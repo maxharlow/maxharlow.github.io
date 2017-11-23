c824c40349817ce65ed2ebf56262a305e1b9d228
Max Harlow <maxharlow@gmail.com>
2017-11-23 09:17:27 +0000

    Update site

--- analytics.js
+++
@@ -1,7 +0,0 @@
-window.GoogleAnalyticsObject = 'ga'
-window.ga = function () {
-    (window.ga.q = window.ga.q || []).push(arguments)
-}
-window.ga.l =+ new Date
-ga('create', 'UA-43951427-1', 'maxharlow.com')
-ga('send', 'pageview')
